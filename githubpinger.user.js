5194648f28d8050a69669d4238a4044418748f16
Andrew Garrett <ajg9132@rit.edu>
2013-10-05 14:20:58 -0400

    add button to page

---
+++ githubpinger.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name GitHubPinger
+// @namespace GitHubPinger
+// @include https://github.com/*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.js
+// @version 0.01
+// ==/UserScript==
+
+function add_button() {
+	this.$buttonNode = $('<a>Message</a>');
+	$buttonNode.val('href', '#');
+	$buttonNode.addClass('minibutton');
+	$('div .tabnav-right').prepend($buttonNode);
+}
+
+add_button();
