a893e658fc31823e5a0b0ed833aced171d02c49d
Andrew Garrett <ajg9132@rit.edu>
2013-10-05 16:50:04 -0400

    hey dollar sign, how's it hanging

--- githubpinger.user.js
+++ githubpinger.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name GitHubPinger
 // @namespace GitHubPinger
-// @include /^https?://github\.com/[0-9A-Za-z]+/
+// @include /^https?://github\.com/[0-9A-Za-z]+$/
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.js
-// @version 0.02
+// @version 0.03
 // ==/UserScript==
 
 function get_username() {
