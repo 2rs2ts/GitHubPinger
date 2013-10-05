1838291dffd666b324edff9f6027f68362ea1649
Andrew Garrett <ajg9132@rit.edu>
2013-10-05 14:56:51 -0400

    log the username to the console
    just a lil' test!

--- githubpinger.user.js
+++ githubpinger.user.js
@@ -3,13 +3,24 @@
 // @namespace GitHubPinger
 // @include /^https?://github\.com/[0-9A-Za-z]+/
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.js
-// @version 0.01
+// @version 0.02
 // ==/UserScript==
+
+function get_username() {
+	return window.location.pathname.replace('/','');
+}
+
+function clicky() {
+	console.log(get_username());
+}
 
 function add_button() {
 	this.$buttonNode = $('<a>Message</a>');
 	$buttonNode.val('href', '#');
 	$buttonNode.addClass('minibutton');
+
+	$buttonNode.click(clicky);
+
 	$('div .tabnav-right').prepend($buttonNode);
 }
 
